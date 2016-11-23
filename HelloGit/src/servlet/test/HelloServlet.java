aacc670371d89764d6172da689a389d35e3034e7
kosta <kosta@kosta-HP>
2016-11-23 10:28:49 +0900

    version2-서블릿 h2 태그를 추가

--- HelloGit/src/servlet/test/HelloServlet.java
+++ HelloGit/src/servlet/test/HelloServlet.java
@@ -20,7 +20,12 @@ public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello....Git");
+		//////////////////2¤º¤¿¼ö Á¤
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h2>Hello....Git..Test</h2>");
+		out.println("</body>");
+		out.println("</html>");
 		out.close();
 	}
 
